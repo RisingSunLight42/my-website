868160733c392dfea9f3700c9a62211a4360e165
RisingSunLight <risingsunlight5776@gmail.com>
2022-03-14 19:19:31 +0100

    Ajout de la fonction navSlide qui récupère les éléments burger et nav-links de la page

---
+++ app.js
@@ -0,0 +1,7 @@
+const navSlide = () => {
+    // document.querySelector permet de récupérer un objet donné, comme une classe par exemple
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+};
+
+navSlide();
