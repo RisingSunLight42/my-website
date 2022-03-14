a3126a7b5f17ae81558e045e575a03c8d2cf5611
RisingSunLight <risingsunlight5776@gmail.com>
2022-03-14 19:28:51 +0100

    Ajout d'un forEach pour commencer l'animation des liens de navigation

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const navSlide = () => {
     // document.querySelector permet de récupérer un objet donné, comme une classe par exemple
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li"); // Récupère tous les li de la classe nav-links
 
-    //* Ajout d'un listener de click sur le burger
+    //* Ajout d'un listener on click pour le burger
     burger.addEventListener("click", () => {
-        //* La liste ci-dessous permet de changer la classe de l'élément en la classe passée en paramètre
+        //* La fonction ci-dessous permet de changer la classe de l'élément en la classe passée en paramètre
         nav.classList.toggle("nav-active");
+
+        //* Animation des liens
+        navLinks.forEach((link, index) => {});
     });
 };
 
