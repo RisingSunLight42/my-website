aaadb1235b9619d82c0acfa16c72cc481803ad77
RisingSunLight <risingsunlight5776@gmail.com>
2022-03-14 19:21:07 +0100

    Début des actions lorsque l'on clique sur le burger

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const navSlide = () => {
     // document.querySelector permet de récupérer un objet donné, comme une classe par exemple
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
+
+    //* Ajout d'un listener de click sur le burger
+    burger.addEventListener("click", () => {
+        //* La liste ci-dessous permet de changer la classe de l'élément en la classe passée en paramètre
+        nav.classList.toggle("nav-active");
+    });
 };
 
 navSlide();
