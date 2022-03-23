837c29203d9b6c87607ef425b8b84e44360b1918
RisingSunLight <risingsunlight5776@gmail.com>
2022-03-23 19:31:48 +0100

    Ajout de la gestion de l'affichage de contenu

--- app.js
+++ app.js
@@ -85,12 +85,28 @@ const ongletChosesAimees = () => {
     onglets.forEach((onglet) => {
         // Pour chaque onglet
         onglet.addEventListener("click", () => {
+            // Modifie l'index, pour pouvoir le lier au bon contenu
+            index = onglet.getAttribute("data-anim");
             // On définit ce qu'il se passe lors du clic
-            if (onglet.classList.contains("active")) {
+            if (onglet.classList.contains("ongletActif")) {
                 // Si l'onglet a la classe active, on ne veut rien faire, donc return
                 return;
             } else {
-                onglet.classList.add("active");
+                for (i = 0; i < onglets.length; i++) {
+                    // Parcours les onglets pour retirer la class ongletActif si présente
+                    onglets[i].classList.remove("ongletActif");
+                }
+                onglet.classList.add("ongletActif"); // Ajoute ensuite la class à l'onglet voulu
+
+                for (j = 0; j < contenu.length; j++) {
+                    // Parcours les contenus et vérifie si leur attribut data-anim correspond à l'index de l'onglet
+                    if (contenu[j].getAttribute("data-anim") == index) {
+                        // Si oui, on l'affiche, sinon on le cache
+                        contenu[j].classList.add("contenuActif");
+                    } else {
+                        contenu[j].classList.remove("contenuActif");
+                    }
+                }
             }
         });
     });
