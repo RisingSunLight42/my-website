4303d8f53a6a575f48e6bc55d4a4e7cc17e64b94
RisingSunLight <risingsunlight5776@gmail.com>
2022-08-21 11:25:57 +0200

    ✅ Add full génération of the cards

--- scripts/for_premid.js
+++ scripts/for_premid.js
@@ -27,12 +27,64 @@ const getMyPresencesDatas = async () => {
 const createDynamicPresencesCard = async () => {
     const presencesArray = await getMyPresencesDatas();
     for (const presence of presencesArray) {
-        const newDiv = document.createElement("div");
-        newDiv.className = "card_front premid";
-        newDiv.style = `background-color: ${presence.metadata.color}`;
+        // Partie carte
+        const divCard = document.createElement("div");
+        divCard.className = "card_front premid";
+        divCard.style = `background-color: ${presence.metadata.color}`;
+
+        // Partie image
+        const divFill = document.createElement("div");
+        divFill.className = "fill";
+        const img = document.createElement("img");
+        img.src = presence.metadata.logo;
+        divFill.appendChild(img);
+
+        //* Création infos
+        const divInfo = document.createElement("div");
+        divInfo.className = "info premid";
+
+        // Partie titre
+        const title = document.createElement("h2");
         const presenceName = document.createTextNode(presence.metadata.service);
-        newDiv.appendChild(presenceName);
-        document.querySelector("main").appendChild(newDiv);
+        title.appendChild(presenceName);
+        divInfo.appendChild(title);
+
+        // Partie paragraphe
+        const description = document.createElement("p");
+        const textDesc = document.createTextNode(
+            presence.metadata.description?.fr ??
+                presence.metadata.description.en
+        );
+        description.appendChild(textDesc);
+        divInfo.appendChild(description);
+
+        // Partie stats
+        const divStats = document.createElement("div");
+        const [paragrapheCategorie, paragrapheVersion, paragrapheUsers] = [
+            document.createElement("p"),
+            document.createElement("p"),
+            document.createElement("p"),
+        ];
+        const [textCategorie, textVersion, textUsers] = [
+            document.createTextNode(
+                `Catégorie : ${presence.metadata.category}`
+            ),
+            document.createTextNode(`Version : ${presence.metadata.version}`),
+            document.createTextNode(`Utilisateurs : ${presence.users}`),
+        ];
+        paragrapheCategorie.appendChild(textCategorie);
+        paragrapheVersion.appendChild(textVersion);
+        paragrapheUsers.appendChild(textUsers);
+        divStats.appendChild(paragrapheCategorie);
+        divStats.appendChild(paragrapheVersion);
+        divStats.appendChild(paragrapheUsers);
+        divInfo.appendChild(divStats);
+
+        // Complétion carte
+        divCard.appendChild(divFill);
+        divCard.appendChild(divInfo);
+
+        document.querySelector("main").appendChild(divCard);
     }
 };
 
