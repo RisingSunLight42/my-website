f509f5a3e5688f48e72e30d72bf9ce6a6675f327
RisingSunLight <risingsunlightdev@gmail.com>
2025-05-30 21:51:49 +0200

    remove: old stuff not worth it

--- scripts/for_tdf.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-fs.readFile("./autre/liste_francais_t.txt", (err, data) => {
-    if (err) throw err;
-
-    arr = data.toString().split("\r\n");
-    document.querySelector("main.centre > div").textContent =
-        arr[Math.round(Math.random() * arr.length)] + " De Fou";
-});
