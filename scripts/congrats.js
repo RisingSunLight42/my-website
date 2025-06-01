e479c11018f9c035c2ee9b181669c6ea3159c149
RisingSunLight <risingsunlightdev@gmail.com>
2025-06-01 11:56:28 +0200

    add: the present

--- scripts/congrats.js
+++ scripts/congrats.js
@@ -1,2 +1,2 @@
-console.log("https://fansheltermcwiki.miraheze.org/wiki/Main_Page");
-console.log("https://pastebin.com/9Qg0pKgB");
+console.log("https://fanshel- ERROR ERROR ERROR");
+console.log("https://pastebin.com/9Qg0pKgB UPDATED");
